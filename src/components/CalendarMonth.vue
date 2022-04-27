b39fd3659b17145062109dd664aaaf3ba45d24aa
Mattia Zanichelli <mattia.zanichelli@student.supsi.ch>
2022-04-27 15:43:18 +0200

    completed reactive calendar

--- src/components/CalendarMonth.vue
+++ src/components/CalendarMonth.vue
@@ -2,12 +2,12 @@
   <div class="calendar-month">
     <div class="calendar-month-header">
       <CalendarDateIndicator
-        :selected-date="selectedDate"
+        :selected-date="selectedDate.value"
         class="calendar-month-header-selected-month"
       />
       <CalendarDateSelector
         :current-date="today"
-        :selected-date="selectedDate"
+        :selected-date="selectedDate.value"
         @dateSelected="selectDate"
       />
     </div>
@@ -19,7 +19,7 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, reactive } from 'vue'
 import dayjs from "dayjs";
 import weekday from "dayjs/plugin/weekday";
 import weekOfYear from "dayjs/plugin/weekOfYear";
@@ -27,88 +27,82 @@ import CalendarDateIndicator from "./CalendarDateIndicator.vue"
 import CalendarDateSelector from "./CalendarDateSelector.vue"
 import CalendarWeekdays from './CalendarWeekdays.vue';
 import CalendarMonthDayItem from './CalendarMonthDayItem.vue';
+import { data } from 'browserslist';
 
 dayjs.extend(weekday);
 dayjs.extend(weekOfYear);
 
-let selectedDate = ref( dayjs() )
+let selectedDate = reactive ({
+  value: dayjs()
+});
+
 let today = ref( dayjs().format("YYYY-MM-DD") )
 
-const days = ([
-        { date: "2020-06-29", isCurrentMonth: false },
-        { date: "2020-06-30", isCurrentMonth: false },
-        { date: "2020-07-01", isCurrentMonth: true },
-        { date: "2020-07-02", isCurrentMonth: true },
-        { date: "2020-07-03", isCurrentMonth: true },
-        { date: "2020-07-04", isCurrentMonth: true },
-        { date: "2020-07-05", isCurrentMonth: true },
-        { date: "2020-07-06", isCurrentMonth: true },
-        { date: "2020-07-07", isCurrentMonth: true },
-        { date: "2020-07-08", isCurrentMonth: true },
-        { date: "2020-07-09", isCurrentMonth: true },
-        { date: "2020-07-10", isCurrentMonth: true },
-        { date: "2020-07-11", isCurrentMonth: true },
-        { date: "2020-07-12", isCurrentMonth: true },
-        { date: "2020-07-13", isCurrentMonth: true },
-        { date: "2020-07-14", isCurrentMonth: true },
-        { date: "2020-07-15", isCurrentMonth: true },
-        { date: "2020-07-16", isCurrentMonth: true },
-        { date: "2020-07-17", isCurrentMonth: true },
-        { date: "2020-07-18", isCurrentMonth: true },
-        { date: "2020-07-19", isCurrentMonth: true },
-        { date: "2020-07-20", isCurrentMonth: true },
-        { date: "2020-07-21", isCurrentMonth: true },
-        { date: "2020-07-22", isCurrentMonth: true },
-        { date: "2020-07-23", isCurrentMonth: true },
-        { date: "2020-07-24", isCurrentMonth: true },
-        { date: "2020-07-25", isCurrentMonth: true },
-        { date: "2020-07-26", isCurrentMonth: true },
-        { date: "2020-07-27", isCurrentMonth: true },
-        { date: "2020-07-28", isCurrentMonth: true },
-        { date: "2020-07-29", isCurrentMonth: true },
-        { date: "2020-07-30", isCurrentMonth: true },
-        { date: "2020-07-31", isCurrentMonth: true },
-        { date: "2020-08-01", isCurrentMonth: false },
-        { date: "2020-08-02", isCurrentMonth: false }]);
+const days = computed(() => {
+  return [
+        ...previousMonthDays(),
+        ...currentMonthDays(),
+        ...nextMonthDays()
+      ];
+})
 
-function selectDate(newSelectedDate) {
-  selectedDate.value = newSelectedDate
+function month() {
+  return Number(selectedDate.value.format("M"));
+}
+
+function year() {
+  return Number(selectedDate.value.format("YYYY"));
 }
 
 function numberOfDaysInMonth() {
-  return dayjs(this.selectDate).daysInMonth();
+  return dayjs(selectedDate.value).daysInMonth();
 }
 
 function currentMonthDays() {
-  return [...Array(this.numberOfDaysInMonth)].map((day, index) => {
+  return [...Array(numberOfDaysInMonth())].map((_, index) => {
     return {
-      date: dayjs(`${this.year}-${this.month}-${index + 1}`).format("YYYY-MM_DD"),
+      date: dayjs(`${year()}-${month()}-${index + 1}`).format("YYYY-MM-DD"),
       isCurrentMonth: true
     };
   });
 }
 
-function getToday() {
-  return dayjs().format("YYYY-MM-DD");
-}
+function previousMonthDays() {
+    const firstDayOfTheMonthWeekday = getWeekday(currentMonthDays()[0].date);
+    const previousMonth = dayjs(`${year()}-${month()}-01`).subtract(1, "month");
 
-function getMonth() {
-  return Number(this.selectDate.format("M"));
-}
+    const visibleNumberOfDaysFromPreviousMonth = firstDayOfTheMonthWeekday ? firstDayOfTheMonthWeekday - 1 : 6;
 
-function getYear() {
-  return Number(this.selectDate.format("YYYY"));
-}
+    const previousMonthLastMondayDayOfMonth = dayjs(currentMonthDays()[0].date).subtract(visibleNumberOfDaysFromPreviousMonth, "day").date();
+
+    return [...Array(visibleNumberOfDaysFromPreviousMonth)].map((_, index) => {
+      return {
+        date: dayjs(`${previousMonth.year()}-${previousMonth.month() + 1}-${previousMonthLastMondayDayOfMonth + index}`).format("YYYY-MM-DD"),
+        isCurrentMonth: false
+      };
+    });
+  }
+
+function nextMonthDays() {
+    const lastDayOfTheMonthWeekday = getWeekday(`${year()}-${month()}-${currentMonthDays().length}`);
+    const nextMonth = dayjs(`${year()}-${month()}-01`).add(1, "month");
+    const visibleNumberOfDaysFromNextMonth = lastDayOfTheMonthWeekday ? 7 - lastDayOfTheMonthWeekday : lastDayOfTheMonthWeekday;
+
+    return [...Array(visibleNumberOfDaysFromNextMonth)].map((_, index) => {
+      return {
+        date: dayjs(`${nextMonth.year()}-${nextMonth.month() + 1}-${index + 1}`).format("YYYY-MM-DD"),
+        isCurrentMonth: false
+      };
+    });
+  }
 
 function getWeekday(date) {
   return dayjs(date).weekday();
 }
 
-// TODO return days as a single array
-// function days() {
-//   return [
-//   ]
-// }
+function selectDate(newSelectedDate) {
+  selectedDate.value = newSelectedDate
+}
 
 </script>
 
